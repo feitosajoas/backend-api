cc34b4f9762885793bcdd79c20b3f80dc5fc23fb
Joás Feitosa <joasfeitosadelimasilva@gmail.com>
2021-12-20 23:57:39 -0300

    Criando entidade fechada

--- Manager/src/Site.Domain/Entities/User.cs
+++ Manager/src/Site.Domain/Entities/User.cs
@@ -2,6 +2,8 @@ namespace Site.Domain.Entities
 {
     public class User : Base
     {
-
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
     }
 }
